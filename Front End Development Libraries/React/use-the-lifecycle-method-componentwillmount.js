9a653587fb6bc918ca64ea076a9fd0ab9445760f
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2021-10-25 19:14:56 -0700

    componentwillmount

---
+++ Front End Development Libraries/React/use-the-lifecycle-method-componentwillmount.js
@@ -0,0 +1,15 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  componentWillMount() {
+    // Change code below this line
+      console.log("Inside componentWillMount")
+    // Change code above this line
+  }
+  
+  render() {
+    return <div />
+  }
+};
