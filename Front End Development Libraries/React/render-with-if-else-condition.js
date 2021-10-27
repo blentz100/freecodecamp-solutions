7130e8ddd4f8fdd0679866a116c4ac4a9923be81
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2021-10-27 15:51:06 -0700

    in progress

---
+++ Front End Development Libraries/React/render-with-if-else-condition.js
@@ -0,0 +1,27 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true
+    }
+    this.toggleDisplay = this.toggleDisplay.bind(this);
+  }
+  toggleDisplay() {
+    this.setState((state) => ({
+      display: !state.display
+    }));
+    console.log("this.state.display is: " + this.state.display)
+  }
+  render() {
+    // Change code below this line
+    if (this.state.display){
+    return (
+       <div>
+         <button onClick={this.toggleDisplay}>Toggle Display</button>
+         <h1>Displayed!</h1>
+       </div>
+    );
+    } 
+  }
+ 
+};
