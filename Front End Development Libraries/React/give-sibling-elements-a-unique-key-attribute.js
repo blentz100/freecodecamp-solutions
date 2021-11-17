c728b13a3b19519bdcc5162f4bbcdf5705e5fb16
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2021-11-16 22:12:04 -0700

    easy one. improvement could be better unique key assignment

---
+++ Front End Development Libraries/React/give-sibling-elements-a-unique-key-attribute.js
@@ -0,0 +1,22 @@
+const frontEndFrameworks = [
+  'React',
+  'Angular',
+  'Ember',
+  'Knockout',
+  'Backbone',
+  'Vue'
+];
+
+function Frameworks() {
+  const renderFrameworks = frontEndFrameworks.map(x => {
+    return (<li key={x}>{x}</li>);
+  }); // Change this line
+  return (
+    <div>
+      <h1>Popular Front End JavaScript Frameworks</h1>
+      <ul>
+        {renderFrameworks}
+      </ul>
+    </div>
+  );
+};
