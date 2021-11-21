9e5c1e908d97f79b547e5b6efaa1da531a5b075f
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2021-11-21 09:56:45 -0500

    match actually extracts the matches and returns them, as opposed to just returning true and false

---
+++ Javascript Algorithms and Data Structures/Regular Expressions/extract-matches.js
@@ -0,0 +1,30 @@
+/*
+ * Extract Matches
+So far, you have only been checking if a pattern exists or not within a string.
+You can also extract the actual matches you found with the .match() method.
+
+To use the .match() method, apply the method on a string and pass in the regex
+inside the parentheses.
+
+Here's an example:
+
+"Hello, World!".match(/Hello/); let ourStr = "Regular expressions"; let ourRegex
+= /expressions/; ourStr.match(ourRegex); Here the first match would return
+["Hello"] and the second would return ["expressions"].
+
+Note that the .match syntax is the "opposite" of the .test method you have been
+using thus far:
+
+'string'.match(/regex/); /regex/.test('string'); Apply the .match() method to
+extract the string coding.
+ */
+
+//match actually extracts the matches and returns them
+
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; // Change this line
+let result = extractStr.match(codingRegex); // Change this line
+
+console.log(result);
+
+
