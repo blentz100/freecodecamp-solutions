61417415c325c497d0ddce879ef9cf594f9aeda7
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2021-11-24 07:30:39 -0700

    use * to match a character 0 or more times

---
+++ Javascript Algorithms and Data Structures/Regular Expressions/match-characters-that-occur-zero-or-more-times.js
@@ -0,0 +1,27 @@
+/*
+ * Match Characters that Occur Zero or More Times
+The last challenge used the plus + sign to look for characters that occur one or
+more times. There's also an option that matches characters that occur zero or
+more times.
+
+The character to do this is the asterisk or star: *.
+
+let soccerWord = "gooooooooal!"; let gPhrase = "gut feeling"; let oPhrase =
+"over the moon"; let goRegex = /go*/; soccerWord.match(goRegex);
+gPhrase.match(goRegex); oPhrase.match(goRegex); In order, the three match calls
+would return the values ["goooooooo"], ["g"], and null.
+
+For this challenge, chewieQuote has been initialized as the string
+Aaaaaaaaaaaaaaaarrrgh! behind the scenes. Create a regex chewieRegex that uses
+the * character to match an uppercase A character immediately followed by zero
+or more lowercase a characters in chewieQuote. Your regex does not need flags or
+character classes, and it should not match any of the other quotes.
+*/
+
+// Only change code below this line
+let chewieRegex = /Aa*/; // Change this line
+// Only change code above this line
+
+let result = chewieQuote.match(chewieRegex);
+
+console.log(result)
