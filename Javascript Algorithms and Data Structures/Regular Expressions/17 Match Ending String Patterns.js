4b01ab76fce888e43c93378a02a023ba37a1c0b2
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2021-11-26 19:36:21 -0700

    match a pattern at the end of string with $

---
+++ Javascript Algorithms and Data Structures/Regular Expressions/17 Match Ending String Patterns.js
@@ -0,0 +1,21 @@
+/*
+ * Match Ending String Patterns
+In the last challenge, you learned to use the caret character to search for patterns at the beginning of strings. There is also a way to search for patterns at the end of strings.
+
+You can search the end of strings using the dollar sign character $ at the end of the regex.
+
+let theEnding = "This is a never ending story";
+let storyRegex = /story$/;
+storyRegex.test(theEnding);
+let noEnding = "Sometimes a story will have to end";
+storyRegex.test(noEnding);
+The first test call would return true, while the second would return false.
+
+Use the anchor character ($) to match the string caboose at the end of the string caboose.
+*/
+
+let caboose = "The last car on a train is the caboose";
+let lastRegex = /caboose$/; // Change this line
+let result = lastRegex.test(caboose);
+
+console.log(result)
