fb40ade1efc4bb2525bbd5b51400bd9e59490055
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2021-11-30 22:51:27 -0700

    solved with 3 different groups

--- Javascript Algorithms and Data Structures/Regular Expressions/22 Restrict Possible Usernames.js
+++ Javascript Algorithms and Data Structures/Regular Expressions/22 Restrict Possible Usernames.js
@@ -19,11 +19,18 @@ Change the regex userCheck to fit the constraints listed above.
 // the solution below works for all cases except A1 
 
 let username = "Z97";
-let userCheck = /^[^\d][a-z]{2,}[0-9]*$|^[a-z]{2}$|^[a-z]+\d+$/i; // Change this line
+let userCheck = /^[a-z][a-z]+[0-9]*$|^[a-z]{2,}$|^[a-z][0-9][0-9]$/gi; // Change this line
 let result = userCheck.test(username);
+let match = username.match(userCheck)
 console.log('myresults:')
 console.log(result)
+console.log(match)
 
+
+//this took a few days to solve, probably not the most efficient solution but it works, had to break it up into 3 different groups
 
 //this solves all except alternating letters and characters in the middle
 //let userCheck = /[A-Z]+[a-z0-9]{2}[0-9]*$|^[a-z]{2}$/i; // Change this line
+
+//this fails the J and A1 tests
+//let userCheck = /^[a-z]{1,}[0-9]*$|^[a-z]{2,}$/gi; // Change this line
