a9f5dd8bacc351a79d7069f19c5c9884df6addfc
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2021-12-06 08:04:02 -0700

    solved - closed a missing ] and )

---
+++ Javascript Algorithms and Data Structures/Debugging/05 Catch Unclosed Parentheses, Brackets, Braces and Quotes.js
@@ -0,0 +1,20 @@
+/* Catch Unclosed Parentheses, Brackets, Braces and QuotesPassed
+Another syntax error to be aware of is that all opening parentheses, brackets,
+curly braces, and quotes have a closing pair. Forgetting a piece tends to happen
+when you're editing existing code and inserting items with one of the pair
+types. Also, take care when nesting code blocks into others, such as adding a
+callback function as an argument to a method.
+
+One way to avoid this mistake is as soon as the opening character is typed,
+immediately include the closing match, then move the cursor back between them
+and continue coding. Fortunately, most modern code editors generate the second
+half of the pair automatically.
+
+Fix the two pair errors in the code. */
+
+let myArray = [1, 2, 3];
+let arraySum = myArray.reduce((previous, current) =>  previous + current);
+console.log(`Sum of array values is: ${arraySum}`);
+
+//myArray was missing a closing ]
+//the arguments for the anonymous function were missing a closing )
