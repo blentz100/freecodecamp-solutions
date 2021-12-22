b1112abf5a4b97b5c56939115f2edcff155a2aef
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2021-12-22 08:14:07 -0700

    tweak the algorithm to solve for all negative numbers

--- Javascript Algorithms and Data Structures/Basic Algorithm Scripting/05 Return Largest Numbers in Arrays.js
+++ Javascript Algorithms and Data Structures/Basic Algorithm Scripting/05 Return Largest Numbers in Arrays.js
@@ -9,10 +9,17 @@ each member with array syntax arr[i].
 
 function largestOfFour(arr) {
   let solutionArray = arr.map(group => group.reduce( (largest, current) => {
-    return Math.max(largest,current)}, 0));;
+    return Math.max(largest,current)}, group[0]));;
   return solutionArray;
 
 }
 
 console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
 
+//this test case gave me problems because the very last array had all negative
+//numbers, and that didn't seem to work with the original algorithm I wrote.
+//to solve it, had to change the initial value from 0 to group[0]
+//I discovered I could solve the problem by hardcoding this value to -100 at
+//first, and then figure out I could just use the value of the first element in
+//the array so it will work for any input
+console.log(largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]));
