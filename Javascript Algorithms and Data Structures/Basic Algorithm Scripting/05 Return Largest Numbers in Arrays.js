51c5215776329821ea590b59348e2c43338deab0
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2021-12-22 08:04:34 -0700

    solved with a map function and a reduce function

---
+++ Javascript Algorithms and Data Structures/Basic Algorithm Scripting/05 Return Largest Numbers in Arrays.js
@@ -0,0 +1,25 @@
+/*Return Largest Numbers in Arrays
+Return an array consisting of the largest number from each provided sub-array.
+For simplicity, the provided array will contain exactly 4 sub-arrays.
+
+Remember, you can iterate through an array with a simple for loop, and access
+each member with array syntax arr[i].
+
+*/
+
+function largestOfFour(arr) {
+  let solutionArray = arr.map(group => group.reduce( (largest, current) => {
+    return Math.max(largest,current)}, 0));;
+  return solutionArray;
+
+}
+
+
+  /* const answer = strArray.reduce((longest, current) => {
+    console.log(`longest is: ${longest}`);
+    console.log(`current is: ${current}`);
+    return Math.max(longest,current.length)
+  },0) */
+
+console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+
