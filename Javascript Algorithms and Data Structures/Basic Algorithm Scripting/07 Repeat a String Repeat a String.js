4af16962b588b748e6bf039feb72abe2d02ead89
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2021-12-24 16:29:07 -0700

    solved with for loop

---
+++ Javascript Algorithms and Data Structures/Basic Algorithm Scripting/07 Repeat a String Repeat a String.js
@@ -0,0 +1,14 @@
+/*Repeat a String Repeat a String
+Repeat a given string str (first argument) for num times (second argument).
+Return an empty string if num is not a positive number. For the purpose of this
+challenge, do not use the built-in .repeat() method.
+*/
+
+function repeatStringNumTimes(str, num) {
+  let answer = "";
+    for(let i = 0; i < num; i++){
+      answer += str; 
+    }
+    return answer;
+}
+console.log(repeatStringNumTimes("abc", 0));
