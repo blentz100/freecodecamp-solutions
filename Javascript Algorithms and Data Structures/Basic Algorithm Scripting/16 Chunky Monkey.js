9ecf8e325d12e24582ef5daf4465c0364b43613a
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-01-01 08:23:34 -0700

    solved with nested for loops

--- Javascript Algorithms and Data Structures/Basic Algorithm Scripting/16 Chunky Monkey.js
+++ Javascript Algorithms and Data Structures/Basic Algorithm Scripting/16 Chunky Monkey.js
@@ -4,24 +4,25 @@ Write a function that splits an array (first argument) into groups the length of
 size (second argument) and returns them as a two-dimensional array.
 
 */
-
+//probably could use some refactoring, but it passes all test cases
 
 function chunkArrayInGroups(arr, size) {
-  let newArr = []; 
+  let newArr = [];
 
   for(let i = 0; i < arr.length; ){
     let subArr = [];
-    for(j = 0; j < size; j++){
-        subArr.push(arr[i]);
+    for(let j = 0; j < size; j++){
+        if(arr[i] !== undefined){
+          subArr.push(arr[i]);
+        }
         i++;
     }
     newArr.push(subArr);
   }
 
-  
+
   return newArr;
 }
 
-console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
-
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4));
 
