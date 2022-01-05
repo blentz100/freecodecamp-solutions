66de3ed5e9219f5396be4904850c33f442fead17
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-01-05 07:02:54 -0700

    solved with a for loop

---
+++ Javascript Algorithms and Data Structures/Object Oriented Programming/09 Understand Own Properties.js
@@ -0,0 +1,45 @@
+/*
+ * Understand Own Properties
+In the following example, the Bird constructor defines two properties: name and
+numLegs:
+
+function Bird(name) { this.name  = name; this.numLegs = 2; }
+
+let duck = new Bird("Donald"); let canary = new Bird("Tweety"); name and numLegs
+are called own properties, because they are defined directly on the instance
+object. That means that duck and canary each has its own separate copy of these
+properties. In fact every instance of Bird will have its own copy of these
+properties. The following code adds all of the own properties of duck to the
+array ownProps:
+
+let ownProps = [];
+
+for (let property in duck) { if(duck.hasOwnProperty(property)) {
+ownProps.push(property); } }
+
+console.log(ownProps); The console would display the value ["name", "numLegs"].
+
+Add the own properties of canary to the array ownProps.
+*/
+
+
+function Bird(name) {
+  this.name = name;
+  this.numLegs = 2;
+}
+
+let canary = new Bird("Tweety");
+let ownProps = [];
+// Only change code below this line
+
+for (let property in canary){
+  console.log(property)
+  //not sure why they use this if statement
+  // it does work with or without it
+  // probably just some extra error handling
+  if(canary.hasOwnProperty(property)){
+    ownProps.push(property)
+  }
+}
+
+console.log(ownProps)
