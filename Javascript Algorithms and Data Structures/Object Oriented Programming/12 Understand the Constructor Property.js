7d6b615d1325a91db59f78652de39a4fe996a5bb
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-01-06 08:10:40 -0700

    solved using constructor property

---
+++ Javascript Algorithms and Data Structures/Object Oriented Programming/12 Understand the Constructor Property.js
@@ -0,0 +1,46 @@
+/*
+ * Understand the Constructor Property
+There is a special constructor property located on the object instances duck and
+beagle that were created in the previous challenges:
+
+let duck = new Bird(); let beagle = new Dog();
+
+console.log(duck.constructor === Bird); console.log(beagle.constructor === Dog);
+Both of these console.log calls would display true in the console.
+
+Note that the constructor property is a reference to the constructor function
+that created the instance. The advantage of the constructor property is that
+it's possible to check for this property to find out what kind of object it is.
+Here's an example of how this could be used:
+
+function joinBirdFraternity(candidate) { if (candidate.constructor === Bird) {
+return true; } else { return false; } } Note: Since the constructor property can
+be overwritten (which will be covered in the next two challenges) it’s generally
+better to use the instanceof method to check the type of an object.
+
+Write a joinDogFraternity function that takes a candidate parameter and, using
+the constructor property, return true if the candidate is a Dog, otherwise
+return false.
+*/
+
+
+function Dog(name) {
+  this.name = name;
+}
+
+// Only change code below this line
+function joinDogFraternity(candidate) {
+  console.log(candidate.constructor);
+  if(candidate.constructor === Dog){
+    return true;
+  }
+  else{
+    return false;
+  }
+  
+}
+
+let newBeagle = new Dog("Fred");
+console.log(newBeagle)
+
+joinDogFraternity(newBeagle);
