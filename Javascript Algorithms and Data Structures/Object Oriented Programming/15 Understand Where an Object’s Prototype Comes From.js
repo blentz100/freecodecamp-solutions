999b593ebe4e8a8e517daea5f659a2d9ce815437
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-01-07 07:05:44 -0700

    solved with isPrototypeOf()

---
+++ Javascript Algorithms and Data Structures/Object Oriented Programming/15 Understand Where an Object’s Prototype Comes From.js
@@ -0,0 +1,26 @@
+/*
+ * Understand Where an Object’s Prototype Comes From
+Just like people inherit genes from their parents, an object inherits its
+prototype directly from the constructor function that created it. For example,
+here the Bird constructor creates the duck object:
+
+function Bird(name) { this.name = name; }
+
+let duck = new Bird("Donald"); duck inherits its prototype from the Bird
+constructor function. You can show this relationship with the isPrototypeOf
+method:
+
+Bird.prototype.isPrototypeOf(duck); This would return true.
+
+Use isPrototypeOf to check the prototype of beagle.
+*/
+
+function Dog(name) {
+  this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+// Only change code below this line
+
+console.log(Dog.prototype.isPrototypeOf(beagle));
