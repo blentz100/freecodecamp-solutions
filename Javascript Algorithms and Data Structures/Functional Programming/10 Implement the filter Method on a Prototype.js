ad71d5748dbd209466d38527947b6952d26e1263
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-01-25 11:54:33 -0500

    solved with a for and an if

--- Javascript Algorithms and Data Structures/Functional Programming/10 Implement the filter Method on a Prototype.js
+++ Javascript Algorithms and Data Structures/Functional Programming/10 Implement the filter Method on a Prototype.js
@@ -1,29 +1,34 @@
 /*
- * Return Part of an Array Using the slice Method
-The slice method returns a copy of certain elements of an array. It can take two
-arguments, the first gives the index of where to begin the slice, the second is
-the index for where to end the slice (and it's non-inclusive). If the arguments
-are not provided, the default is to start at the beginning of the array through
-the end, which is an easy way to make a copy of the entire array. The slice
-method does not mutate the original array, but returns a new one.
+ * Implement the filter Method on a Prototype
+You might learn a lot about the filter method if you implement your own version
+of it. It is recommended you use a for loop or Array.prototype.forEach().
 
-Here's an example:
-
-const arr = ["Cat", "Dog", "Tiger", "Zebra"]; const newArray = arr.slice(1, 3);
-newArray would have the value ["Dog", "Tiger"].
-
-Use the slice method in the sliceArray function to return part of the anim array
-given the provided beginSlice and endSlice indices. The function should return
-an array.
+Write your own Array.prototype.myFilter(), which should behave exactly like
+Array.prototype.filter(). You should not use the built-in filter method. The
+Array instance can be accessed in the myFilter method using this.
 
 */
 
-function sliceArray(anim, beginSlice, endSlice) {
+// The global variable
+const s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function(callback) {
   // Only change code below this line
-  return anim.slice(beginSlice,endSlice)
+  const newArray = [];
+  console.log('this is: ' + this)
+
+  for(let i = 0; i < this.length; i++){
+    if(callback(this[i])){
+      newArray.push(this[i]);
+    }
+    
+  }
   // Only change code above this line
-}
+  return newArray;
+};
 
-const inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
-console.log(sliceArray(inputAnim, 1, 3));
+const new_s = s.myFilter(function(item) {
+  return item % 2 === 1;
+});
 
+console.log("new_s is: " + new_s)
