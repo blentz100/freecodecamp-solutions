0dfad09e1d80aeffabbca80b1aa12f3122bbdb90
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-02-13 16:16:38 -0700

    in progress

--- Javascript Algorithms and Data Structures/Intermediate Algorithm Scripting/04 Wherefore art thou.js
+++ Javascript Algorithms and Data Structures/Intermediate Algorithm Scripting/04 Wherefore art thou.js
@@ -14,11 +14,16 @@ that was passed on as the second argument.
 function whatIsInAName(collection, source) {
   const arr = [];
   let propertyMatchCounter = 0;
-  console.log('source is: ' + JSON.stringify(source) + '\n')
-  // Only change code below this line
+  console.log('source is: ' + JSON.stringify(source))
+  
+  // Iterate over the entire collection that is passed in
   for(let i = 0; i < collection.length; i++){
     console.log('\ncollection[' + i + '] is: ' + JSON.stringify(collection[i]))
-    console.log('Object.keys(collection[i].length is: ' + Object.keys(collection[i]).length);
+    console.log('Object.keys(collection[i].length) is: ' + Object.keys(collection[i]).length);
+
+    //reset property match counter
+    propertyMatchCounter = 0;
+
     //need to iterate over each of the key value pairs in source
     for(let name in source){
       console.log('name is: ' + name)
@@ -27,19 +32,31 @@ function whatIsInAName(collection, source) {
       //then check to see if each one is in the collection[i]
       //if yes, then push collection[i] to the soluttion array
 
-      
-      if(collection[i].hasOwnProperty(name)){
-        console.log('single match!');
-        propertyMatchCounter++; 
+      if(collection[i].hasOwnProperty(name) && source[name] == collection[i][name]){
+        console.log('we found one match, so increment counter, but we need to keep checking to make sure all match');
+        console.log(source[name]);
+        console.log(collection[i][name])
+        propertyMatchCounter++;
       }
+
+      //this should only fire if all props in source match collection[i]
+      // in this example were are getting in here on the collection[1] iteration but shouldn't be
+      // the reason is that when we only have one source key/value pair, it can be a false positive
       if(propertyMatchCounter === Object.keys(collection[i]).length) {
         arr.push(collection[i]);
-        console.log('total match');
+        console.log('FULL MATCH!');
       }
+      
     }
   }
   return arr;
 }
 
-whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1,
-"bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 });
+console.log(whatIsInAName([
+  { "apple": 1, "bat": 2 }, 
+  { "bat": 2 }, 
+  { "apple": 1, "bat": 2, "cookie": 2 }], 
+  
+  { "apple": 1, "bat": 2 }));
+
+
