647445b201dd3a054b1a67d2d92225327bdd1ef5
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-02-14 08:13:04 -0700

    in progress

--- Javascript Algorithms and Data Structures/Intermediate Algorithm Scripting/05 Spinal Tap Case.js
+++ Javascript Algorithms and Data Structures/Intermediate Algorithm Scripting/05 Spinal Tap Case.js
@@ -4,26 +4,43 @@ Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-da
 
 
 
-
 function spinalCase(str) {
 
   //iterate over the string and add dashes if the case changes to break it up into words
+  let solutionStr = str;
 
   for(let i = 0; i < str.length; i++){
+    let currentLetter = str[i];
+    let nextLetter = str[i+1];
+
     let currentLetterCase = "";
+    let nextLetterCase = "";
+
     if(str[i] == str[i].toLowerCase()){
       currentLetterCase = 'lower';
     }
     else{
       currentLetterCase = 'upper'
     }
-    console.log('current letter is: ' + str[i] + ". and the case is: " + currentLetterCase);
 
-    //using this same logic, check the case of the next letter
-    //if there is a mismatch, insert a dash or space
-    
-  
+    if(str[i+1] == str[i+1].toLowerCase()){
+      nextLetterCase = 'lower';
+    }
+    else{
+      nextLetterCase = 'upper'
+    }
+    //console.log('current letter is: ' + currentLetter + ". and the case is: " + currentLetterCase);
+    //console.log('next letter is: ' + nextLetter + ". and the case is: " + nextLetterCase);
 
+    //if current is lower and next is upper, we need to insert a dash
+    if(currentLetterCase == "lower" && nextLetterCase == 'upper'){
+      console.log('now we need to insert a dash')
+      solutionStr = solutionStr.slice(0,i+1) + "-" + solutionStr.slice(i+1);
+      
+      console.log('*******')
+      console.log(solutionStr)
+      console.log('*******')
+    }
 
   }
   //return str.split(/\/).join("-").toLowerCase();
