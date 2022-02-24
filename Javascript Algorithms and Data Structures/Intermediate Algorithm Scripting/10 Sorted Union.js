bfb77ec6eb61b9727487694bb19f41b65817235d
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-02-24 08:56:40 -0700

    added some diagnostics

--- Javascript Algorithms and Data Structures/Intermediate Algorithm Scripting/10 Sorted Union.js
+++ Javascript Algorithms and Data Structures/Intermediate Algorithm Scripting/10 Sorted Union.js
@@ -1,10 +1,13 @@
 /*
  * Sorted Union
-Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+Write a function that takes two or more arrays and returns a new array of unique
+values in the order of the original provided arrays.
 
-In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+In other words, all values present from all arrays should be included in their
+original order, but with no duplicates in the final array.
 
-The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+The unique numbers should be sorted by their original order, but the final array
+should not be sorted in numerical order.
 
 Check the assertion tests for examples.
 */
@@ -19,20 +22,27 @@ function uniteUnique(arr) {
   let tempArray = [];
 
   for(let i = 0; i < arr.length - 1; i++){
-    console.log('********* top of for loop ****** ')
-    console.log(arguments[i]);
+    console.log('** top of outer for loop ** ')
+    console.log("attempting to join: " + arguments[i] + " with: " + arguments[i+1]);
 
     for(let j = 0; j < arguments[i].length; j++){
-  
-      //check for unique values between two arrays
+      console.log("\t * top of inner for loop *"); 
+      //here's the algorithm: take everything in the first array and that gets
+      //included. 
+      //tempArray = arguments[i];
+      
+      //Then, add everything from the second array to that, unless
+      //it's a duplicate
       if(!arguments[i+1].includes(arguments[i][j])){
         //push that unique value into a temp array
         tempArray.push(arguments[i][j])
-        console.log('found a unique value and pushing into tempArray: ', arguments[i][j])
+        console.log('\tfound a unique value and pushing into tempArray: ', arguments[i][j])
       }
+      console.log("\t * bottom of inner for loop *"); 
     }
+
     console.log("tempArray is ", tempArray)
-  console.log("*******bottom of the for loop********")
+  console.log("** bottom of outer for loop **\n")
   }
 
   return arr;
