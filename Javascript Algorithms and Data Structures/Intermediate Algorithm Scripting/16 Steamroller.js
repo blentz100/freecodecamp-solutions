4577ad5e9c2188767af37b5ad66f32cc20453c81
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-03-26 10:53:31 -0700

    in progress

--- Javascript Algorithms and Data Structures/Intermediate Algorithm Scripting/16 Steamroller.js
+++ Javascript Algorithms and Data Structures/Intermediate Algorithm Scripting/16 Steamroller.js
@@ -4,29 +4,28 @@ Flatten a nested array. You must account for varying levels of nesting.
 
 */
 
-function steamrollArray(arr) {
 let flatArr = [];
+
+function steamrollArray(arr) {
+
   for(let i = 0; i < arr.length; i++){
     console.log("--    top of loop number " + i)
-    console.log("arr[i] is:" , arr[i])
+    console.log("arr["+i+"] is:" , arr[i])
     //if arr[i] !== object, then push it into flattened array
     if(typeof arr[i] !== "object" ){
-      console.log('arr[i] is not an object, so pushing it to flatArr')
+      console.log('arr['+i+'] is not an object, so pushing it to flatArr')
       flatArr.push(arr[i])
+      console.log('flatArr is: ' + flatArr)
     }
-
     //if arr[i] == object, then we need to go one level down somehow
-      if(typeof arr[i] == "object"){
-        console.log('arr[i] is an object')
-        steamrollArray(arr[i])
-      }
-  
-
-
+    if(typeof arr[i] == "object"){
+        console.log('arr['+i+'] is an object, so calling steamrollArray(arr[i])')
+        steamrollArray(arr[i]);
+    }
     console.log("-- bottom of loop number " + i + "\n")
   }
-  
+  console.log('return flatArr')
   return flatArr;
 }
 
-console.log(steamrollArray([1,[2]]));
+console.log(steamrollArray([["a"]], [["b"]]));
