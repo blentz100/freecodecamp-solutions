70dce83fdd996bb74ecedfd5170129b97cc4ea93
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-03-30 08:06:08 -0700

    solving the shorter test cases

--- Javascript Algorithms and Data Structures/JavaScript Algorithms and Data Structures Projects/02 Roman Numeral Converter.js
+++ Javascript Algorithms and Data Structures/JavaScript Algorithms and Data Structures Projects/02 Roman Numeral Converter.js
@@ -3,23 +3,39 @@
 All roman numerals answers should be provided in upper-case.
   */
 
-
 function convertToRoman(num) {
   let ans = "";
   let numArray = num.toString().split("");
-  let revArray = numArray.reverse()
-  console.log(revArray)
+  console.log(numArray)
 
-  let thousands = revArray[0];
-  let hundreds = revArray[1];
-  let tens = revArray[2];
-  let ones = revArray[3];
+  let ones = parseInt(numArray.pop(), 10);
+  console.log('ones:' + ones)
 
-  console.log(hundreds)
+  let tens = parseInt(numArray.pop(), 10) * 10;
+  console.log('tens:' + tens)
+
+  let hundreds = parseInt(numArray.pop(), 10) * 100;
+  console.log('hundreds:' + hundreds)
+
+  let thousands = parseInt(numArray.pop(), 10) * 1000;
+  console.log('thousands:' + thousands)
+
+  let onesObject = {1: "I", 2: "II", 3: "III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8: "VIII", 9: "IX"}
+
+  let tensObject = {10: "X", 20: "XX", 30: "XXX", 40: "XL", 50: "L", 6: "LX", 7: "LXX", 8: "LXXX", 9: "XC"}
+
+  let onesFinal = onesObject[ones]
+  console.log(onesFinal)
+
+  let tensFinal = tensObject[tens]
+  console.log(tensFinal)
+
+  ans = tensFinal + onesFinal;
 
   
 
- return num;
+ return ans;
 }
 
-convertToRoman(891);
+console.log(convertToRoman(2));
+
