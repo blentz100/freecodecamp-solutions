29afa29d115d3a85b3ba4ad4c030c8675a1cd6e9
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-04-01 08:12:54 -0700

    solved

--- Javascript Algorithms and Data Structures/JavaScript Algorithms and Data Structures Projects/02 Roman Numeral Converter.js
+++ Javascript Algorithms and Data Structures/JavaScript Algorithms and Data Structures Projects/02 Roman Numeral Converter.js
@@ -20,21 +20,32 @@ function convertToRoman(num) {
   let thousands = parseInt(numArray.pop(), 10) * 1000;
   console.log('thousands:' + thousands)
 
-  let onesObject = {1: "I", 2: "II", 3: "III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8: "VIII", 9: "IX"}
+  let onesObject = {0: "", 1: "I", 2: "II", 3: "III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8: "VIII", 9: "IX"}
 
-  let tensObject = {10: "X", 20: "XX", 30: "XXX", 40: "XL", 50: "L", 6: "LX", 7: "LXX", 8: "LXXX", 9: "XC"}
+  let tensObject = {0: "", 10: "X", 20: "XX", 30: "XXX", 40: "XL", 50: "L", 60: "LX", 70: "LXX", 80: "LXXX", 90: "XC"}
+
+  let hundredsObject = {0: "", 100: "C", 200: "CC", 300: "CCC", 400: "CD", 500: "D", 600: "DC", 700: "DCC", 800: "DCCC", 900: "CM"}
+
+  let thousandsObject = {0: "", 1000: "M", 2000: "MM", 3000: "MMM", 4000: "MV", 5000: "V", 6000: "VM", 7000: "VMM", 8000: "VMMM", 9000: "MX"}
 
   let onesFinal = onesObject[ones]
-  console.log(onesFinal)
+  console.log('onesFinal', onesFinal)
 
   let tensFinal = tensObject[tens]
-  console.log(tensFinal)
+  console.log('tensFinal', tensFinal)
 
-  ans = tensFinal + onesFinal;
+  let hundredsFinal = hundredsObject[hundreds]
+  console.log('hundredsFinal', hundredsFinal)
 
-  
+  let thousandsFinal = thousandsObject[thousands]
+  console.log('thousandsFinal', thousandsFinal)
 
- return ans;
+  ans = thousandsFinal + hundredsFinal + tensFinal + onesFinal;
+
+  let finalAnswer = ans.replace(/undefined/, "");
+  let finalFinalAnswer = finalAnswer.replace(/NaN/, "");
+
+  return finalFinalAnswer;
 }
 
 console.log(convertToRoman(2));
