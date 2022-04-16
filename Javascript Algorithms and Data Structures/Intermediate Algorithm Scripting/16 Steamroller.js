66829dcfb17a50d06747c7a94b52ba5cd83a5fed
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-04-16 13:53:59 -0700

    it's working for a single element array

--- Javascript Algorithms and Data Structures/Intermediate Algorithm Scripting/16 Steamroller.js
+++ Javascript Algorithms and Data Structures/Intermediate Algorithm Scripting/16 Steamroller.js
@@ -4,9 +4,9 @@ Flatten a nested array. You must account for varying levels of nesting.
 
 */
 
-let flatArr = [];
 function steamrollArray(arr) {
-  for(let i = 0; i < arr.length; i++){
+  let flatArr = [];
+  for(let i = 0; i <= arr.length; i++){
     console.log("\n--Top of loop number " + i + ". arr["+i+"] is:" , arr[i])
 
     //we need to start with our base case
@@ -18,16 +18,14 @@ function steamrollArray(arr) {
     if(Array.isArray(arr[i]) == false ){
       console.log('\tarr['+i+'] is not an array, so pushing it to flatArr')
       return(arr[i])
-      flatArr.push(arr[i])
-      console.log('\tflatArr is: ' + flatArr)
     }
 
    //if arr[i] is an array, then call steamrollArray(arr[i])
-    if(Array.isArray(arr[i]) == true ){
+    if(Array.isAray(arr[i]) == true ){
         console.log('\tarr['+i+'] is an array, so calling steamrollArray(arr[i])')
-        flatArr.push(steamrollArray(arr[i]));
+        return steamrollArray(arr[i]);
     }
-    
+
     console.log("--Bottom of loop number " + i + "\n")
   }
   console.log('\treturning flatArr and it is: ')
@@ -35,4 +33,4 @@ function steamrollArray(arr) {
   return flatArr;
 }
 
-console.log(steamrollArray([[[["a"]]]]));
+console.log(steamrollArray([[["a"]]],["b"]));
