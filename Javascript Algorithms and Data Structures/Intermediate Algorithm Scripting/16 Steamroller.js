651ac7e816328b5fc7402c003455c04e126480b6
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-04-18 07:49:57 -0700

    solved, all test cases passing

--- Javascript Algorithms and Data Structures/Intermediate Algorithm Scripting/16 Steamroller.js
+++ Javascript Algorithms and Data Structures/Intermediate Algorithm Scripting/16 Steamroller.js
@@ -8,6 +8,7 @@ let flatArr = [];
 
 function steamrollArray(arr) {
 
+  //this was my start a recursive solution but couldn't get it working
   /*
   for(let i = 0; i < arr.length; i++){
     console.log("--    top of loop number " + i)
@@ -29,36 +30,45 @@ function steamrollArray(arr) {
   return flatArr;
   */
 
-console.log('arr is: ', arr)
+  //this solution below works
+  console.log('arr is: ', arr)
 
-let testArr = Array.from(arguments);
-console.log('testArr is: ', testArr)
-let testAnswer = testArr.join()
-console.log('testAnswer is: ', testAnswer)
+  let testArr = Array.from(arguments);
+  console.log('testArr is: ', testArr)
+  let testAnswer = testArr.join()
+  console.log('testAnswer is: ', testAnswer)
 
-let testAnswer2 = testAnswer.split(",")
-console.log('testAnswer2 is: ', testAnswer2)
+  let testAnswer2 = testAnswer.split(",")
+  console.log('testAnswer2 is: ', testAnswer2)
 
+  let finalAnswer = [];
+  //loop through the whole testAnswer2, if we see numbers, convert them to integers
+  for(let i = 0; i < testAnswer2.length; i++){
+    
+    // case for numbers
+    if(testAnswer2[i].charCodeAt() >= 48 && testAnswer2[i].charCodeAt() <= 58) {
+      finalAnswer.push(parseInt(testAnswer2[i], 10))
+    }
 
-//loop through the whole testAnswer2, if we see numbers, convert them to integers
-for(let i = 0; i < testAnswer2.length; i++){
-  console.log(testAnswer2[i].charCodeAt())
-  if(testAnswer2[i].charCodeAt() >= 48 && testAnswer2[i].charCodeAt() <= 58) {
-    testAnswer2[i] = (parseInt(testAnswer2[i], 10))
+    // add a case for characters here like a and b
+      if(testAnswer2[i].charCodeAt() >= 97 && testAnswer2[i].charCodeAt() <= 122) {
+      finalAnswer.push(testAnswer2[i])
+    }
+    
+    // add a case for the empty array(don't think this is needed)
+    
+    // add a case for the empty object 
+    if(testAnswer2[i].charCodeAt() == 91) {
+      finalAnswer.push({})
+    }
   }
-  // add a case for characters here like a and b
+  console.log('finalAnswer is: ', finalAnswer)
 
-  // add a case for the empty array
-
-  // add a case for the empty object
-}
-console.log('testAnswer2 is: ', testAnswer2)
-
-return testAnswer2;
+  return finalAnswer;
   
 }
 
-console.log(steamrollArray([1, [], [3, [[4]]]]));
+console.log(steamrollArray([1, {}, [3, [[4]]]]));
 console.log('\n')
 
 
