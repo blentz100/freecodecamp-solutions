295326a0b4689794c92895e90c8f8f55b4872e17
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-05-01 06:35:39 -0700

    solved

---
+++ Front End Development Libraries/Bootstrap/20 House our page within a Bootstrap container-fluid div.js
@@ -0,0 +1,10 @@
+/*
+House our page within a Bootstrap container-fluid div
+Now let's make sure all the content on your page is mobile-responsive.
+
+Let's nest your h3 element within a div element with the class container-fluid.
+*/ 
+
+<div class="container-fluid">
+<h3 class="text-primary text-center">jQuery Playground</h3>
+</div>
