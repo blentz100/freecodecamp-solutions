c1ba6a2a96e83b54b0850c6f2b87cd0ff991ee48
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-05-01 06:45:47 -0700

    solved

---
+++ Front End Development Libraries/Bootstrap/21 Create a Bootstrap Row.js
@@ -0,0 +1,13 @@
+/*Create a Bootstrap Row
+Now we'll create a Bootstrap row for our inline elements.
+
+Create a div element below the h3 tag, with a class of row.
+*/ 
+
+<div class="container-fluid">
+  <h3 class="text-primary text-center">jQuery Playground</h3>
+  <div class="row">
+    i'm in a row
+    </div>
+
+</div>
