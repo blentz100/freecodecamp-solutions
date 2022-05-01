bc16e834d320a22bb964b19e0c88c7f8a6ec979d
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-05-01 06:32:10 -0700

    solved

---
+++ Front End Development Libraries/Bootstrap/19 Create a Bootstrap Headline.js
@@ -0,0 +1,13 @@
+/*
+Create a Bootstrap Headline
+Now let's build something from scratch to practice our HTML, CSS and Bootstrap skills.
+
+We'll build a jQuery playground, which we'll soon put to use in our jQuery challenges.
+
+To start with, create an h3 element, with the text jQuery Playground.
+
+Color your h3 element with the text-primary Bootstrap class, and center it with the text-center Bootstrap class.
+*/ 
+
+<h3 class ="text-center text-primary">jQuery Playground</h3>
+
