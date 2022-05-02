15a90ffaef261e23a59e247b20f32659a107f1e2
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-05-02 07:43:13 -0700

    solved

---
+++ Front End Development Libraries/Bootstrap/22 Split Your Bootstrap Row.js
@@ -0,0 +1,17 @@
+/* Split Your Bootstrap Row
+Now that we have a Bootstrap Row, let's split it into two columns to house our elements.
+
+Create two div elements within your row, both with the class col-xs-6.
+*/ 
+
+<div class="container-fluid">
+  <h3 class="text-primary text-center">jQuery Playground</h3>
+  <div class="row">
+    <div class="col-xs-6">
+      col-xs-6
+    </div>
+    <div class="col-xs-6">
+      col-xs-6
+    </div>
+  </div>
+</div>
