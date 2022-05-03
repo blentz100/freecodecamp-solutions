3fea2b73ff7b3ed3b3a3ff4761ec266e22fb2556
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-05-03 09:05:06 -0700

    solved

---
+++ Front End Development Libraries/Bootstrap/30 Label Bootstrap Buttons.js
@@ -0,0 +1,28 @@
+/*
+Label Bootstrap Buttons
+Just like we labeled our wells, we want to label our buttons.
+
+Give each of your button elements text that corresponds to its id selector.
+*/ 
+
+<div class="container-fluid">
+  <h3 class="text-primary text-center">jQuery Playground</h3>
+  <div class="row">
+    <div class="col-xs-6">
+      <h4>#left-well</h4>
+      <div class="well" id="left-well">
+        <button class="btn btn-default target" id="target1">#target1</button>
+        <button class="btn btn-default target" id="target2">#target2</button>
+        <button class="btn btn-default target" id="target3">#target3</button>
+      </div>
+    </div>
+    <div class="col-xs-6">
+      <h4>#right-well</h4>
+      <div class="well" id="right-well">
+        <button class="btn btn-default target" id="target4">#target4</button>
+        <button class="btn btn-default target" id="target5">#target5</button>
+        <button class="btn btn-default target" id="target6">#target6</button>
+      </div>
+    </div>
+  </div>
+</div>
