62fb14f330e0027e63710e4ca996bfa2ddbce011
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-05-03 09:06:37 -0700

    solved

---
+++ Front End Development Libraries/Bootstrap/31 Use Comments to Clarify Code.js
@@ -0,0 +1,33 @@
+/*
+Use Comments to Clarify Code
+When we start using jQuery, we will modify HTML elements without needing to actually change them in HTML.
+
+Let's make sure that everyone knows they shouldn't actually modify any of this code directly.
+
+Remember that you can start a comment with <!-- and end a comment with -->
+
+Add a comment at the top of your HTML that says Code below this line should not be changed
+*/ 
+
+<!-- Code below this line should not be changed -->
+<div class="container-fluid">
+  <h3 class="text-primary text-center">jQuery Playground</h3>
+  <div class="row">
+    <div class="col-xs-6">
+      <h4>#left-well</h4>
+      <div class="well" id="left-well">
+        <button class="btn btn-default target" id="target1">#target1</button>
+        <button class="btn btn-default target" id="target2">#target2</button>
+        <button class="btn btn-default target" id="target3">#target3</button>
+      </div>
+    </div>
+    <div class="col-xs-6">
+      <h4>#right-well</h4>
+      <div class="well" id="right-well">
+        <button class="btn btn-default target" id="target4">#target4</button>
+        <button class="btn btn-default target" id="target5">#target5</button>
+        <button class="btn btn-default target" id="target6">#target6</button>
+      </div>
+    </div>
+  </div>
+</div>
