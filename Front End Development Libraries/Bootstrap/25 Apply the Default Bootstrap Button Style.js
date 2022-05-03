b05d99f5598061926df19f0c36ca422fe31b9dde
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-05-03 08:56:21 -0700

    solved

---
+++ Front End Development Libraries/Bootstrap/25 Apply the Default Bootstrap Button Style.js
@@ -0,0 +1,26 @@
+/*
+Apply the Default Bootstrap Button Style
+Bootstrap has another button class called btn-default.
+
+Apply both the btn and btn-default classes to each of your button elements.
+*/ 
+
+<div class="container-fluid">
+  <h3 class="text-primary text-center">jQuery Playground</h3>
+  <div class="row">
+    <div class="col-xs-6">
+      <div class="well">
+        <button class="btn btn-default"></button>
+        <button class="btn btn-default"></button>
+        <button class="btn btn-default"></button>
+      </div>
+    </div>
+    <div class="col-xs-6">
+      <div class="well">
+        <button class="btn btn-default"></button>
+        <button class="btn btn-default"></button>
+        <button class="btn btn-default"></button>
+      </div>
+    </div>
+  </div>
+</div>
