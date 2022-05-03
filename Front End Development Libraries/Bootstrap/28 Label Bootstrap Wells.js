b1b83d28ce418b1e89fd8496a01e76c042770275
Brendan Lentz <66037960+blentz100@users.noreply.github.com>
2022-05-03 09:01:19 -0700

    solved

---
+++ Front End Development Libraries/Bootstrap/28 Label Bootstrap Wells.js
@@ -0,0 +1,33 @@
+/*
+Label Bootstrap Wells For the sake of clarity, let's label both of our wells
+with their ids.
+
+Above your left-well, inside its col-xs-6 div element, add a h4 element with
+the text #left-well.
+
+Above your right-well, inside its col-xs-6 div element, add a h4 element with
+the text #right-well.
+*/ 
+
+<div class="container-fluid">
+  <h3 class="text-primary text-center">jQuery Playground</h3>
+  <div class="row">
+    <div class="col-xs-6">
+      <h4>#left-well</h4>
+
+      <div class="well" id="left-well">
+        <button class="btn btn-default target"></button>
+        <button class="btn btn-default target"></button>
+        <button class="btn btn-default target"></button>
+      </div>
+    </div>
+    <div class="col-xs-6">
+      <h4>#right-well</h4>
+      <div class="well" id="right-well">
+        <button class="btn btn-default target"></button>
+        <button class="btn btn-default target"></button>
+        <button class="btn btn-default target"></button>
+      </div>
+    </div>
+  </div>
+</div>
